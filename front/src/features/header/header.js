895fd353028c023e5ddb5c87924e95cc1a212b74
tjamard <theo.jamard@etudiant.univ-lr.fr>
2022-03-16 12:15:42 +0100

    feat(US1) : update header #1

--- front/src/features/header/header.js
+++ front/src/features/header/header.js
@@ -2,7 +2,9 @@ const Header = () => {
     return (
         <>
             <div className="header">
-
+                <div className="titleHeader">
+                    Bassin à flot histoire et témoignages
+                </div>
             </div>
         </>
     )

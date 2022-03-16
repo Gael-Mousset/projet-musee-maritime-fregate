8713648e92a039fdf908748bc9df85687a22dd6b
tjamard <theo.jamard@etudiant.univ-lr.fr>
2022-03-16 12:07:27 +0100

    feat(App.js) : add routes

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="*" />
+            <Route path="/" />
+            <Route path="/meteo" />
+            <Route path="/galerie_photos" />
+            <Route path="/liste_bateaux" />
+            <Route path="/bateau/{?id}" />
+            <Route path="/bateau/{?id}/planning" />
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 

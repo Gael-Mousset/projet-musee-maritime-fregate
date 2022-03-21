ae07e2aff554f7b6f7d870f383cf94c693088a86
tjamard <theo.jamard@etudiant.univ-lr.fr>
2022-03-21 10:50:23 +0100

    refactor (US1) : update routage and features directory

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from "./features/home";
+import Layout from "./features/layout/layout";
+import Home from "./features/home/home";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <div className="App">
-              <Routes>
-                  <Route path="/" element={<Home/>} />
-              </Routes>
-          </div>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home/>} />
+                        <Route path="weather"/>
+                        <Route path="picture_gallery"/>
+                        <Route path="boats">
+                            <Route index/>
+                            <Route path=":boatId">
+                                <Route index/>
+                                <Route path="schedule"/>
+                            </Route>
+                        </Route>
+                    </Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- front/src/features/home/home.js
+++ front/src/features/home/home.js
@@ -1,4 +1,4 @@
-import Header from "./header/header";
+import Header from "../header/header";
 import PracticalInfo from "./practicalInfo";
 
 const Home = () => {

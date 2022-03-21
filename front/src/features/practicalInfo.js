fed0ea8260dc965f8e73241c0fbae897b20d92f4
tjamard <theo.jamard@etudiant.univ-lr.fr>
2022-03-21 09:43:47 +0100

    feat (US1) : add meteo #1

--- front/src/features/practicalInfo.js
+++ front/src/features/practicalInfo.js
@@ -1,9 +1,21 @@
 const PracticalInfo = () => {
     return (
         <>
-            <div className="infoContainer">
-                <div className="infoTitleContainer">
+            <div className="contentContainer">
+                <div className="contentTitleContainer">
                     <p>Infos Pratiques</p>
+                </div>
+                <div className="meteoContainer">
+                    <div className="meteo">
+                        <p>Lundi</p>
+                        <img src="" alt="La météo"/>
+                        <p>16°</p>
+                    </div>
+                    <div className="meteo">
+                        <p>Lundi</p>
+                        <img src="" alt="La météo"/>
+                        <p>16°</p>
+                    </div>
                 </div>
                 <p className="infoMusee">Le musée est <span>ouvert</span> aujourd'hui !</p>
             </div>

02e76072c7ae5f684e0ae1377842cf4a50604060
Hugo Magnan <hugo.magnan@etudiant.univ-lr.fr>
2022-03-30 11:03:04 +0200

    Fix (US2) : added "Monter à bord" with a emoticon validate and close

--- front/src/features/home/map.js
+++ front/src/features/home/map.js
@@ -22,6 +22,7 @@ const Map = () => {
             .then(response => response.json())
             .then((value) => {
                 setBoats(value);
+                console.log(value);
             })
         },
         [])
@@ -30,11 +31,21 @@ const Map = () => {
 
 
     const renderedBoats = boats.map((boat, index )=>{
+        let comeInBoat;
+        if (boat.isAvailable == true)
+        {
+            comeInBoat = <p>Monter à bord : <img src="images/Sign-check-icon.png" id={'validate'}/></p>;
+        }
+        else
+        {
+            comeInBoat = <p>Monter à bord : <img src="images/close_box_red.png" id={'close'} /></p>;
+        }
             return(
                 <Marker key={index} position={[boat.lat,boat.lon]}>
                     <Popup className="request-popup">
                         <div className="titleBoat"> <a href={"https://takeb1nzyto.space/"}>{boat.name}</a></div>
                         <p >{boat.description}</p>
+                        {comeInBoat}
                     </Popup>
                 </Marker>
 
